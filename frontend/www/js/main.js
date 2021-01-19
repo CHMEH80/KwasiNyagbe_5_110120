a62056a6a27314b7b51e8aa4ee7baadf2cec95d6
Lionel <fullstackbeaver@gmail.com>
2021-01-19 14:24:08 +0100

    séance du 19 janvier

--- frontend/www/js/main.js
+++ frontend/www/js/main.js
@@ -1,1 +1,5 @@
 const dataManager = new DataManager("http://localhost:3000/api/teddies")
+
+// new ListeProduits(document.getElementById("listeProduit"));
+
+// new FicheProduit("5be9c8541c9d440000665243",document.getElementById("listeProduit"))
---
+++ frontend/www/js/pages/ficheProduit.js
@@ -0,0 +1,9 @@
+class FicheProduit{
+  constructor(idProduit){
+    this.idProduit = idProduit;
+    this.showPage();
+  }
+  showPage(){
+    
+  }
+}
